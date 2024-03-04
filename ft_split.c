d79c394f1ba04b345ddc89ee70e7013149f27041
Cristian Zapata <theposidesign@gmail.com>
2024-03-04 11:19:49 +0100

    upload split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: crizapat <crizapat@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 13:50:24 by crizapat          #+#    #+#             */
-/*   Updated: 2024/03/04 09:56:11 by crizapat         ###   ########.fr       */
+/*   Updated: 2024/03/04 11:19:26 by crizapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,30 @@ static char	**free_blocks(int i, char **block)
 		free(block[i--]);
 	free(block);
 	return (0);
+}
+
+static char **fill_blocks(const char *s, char c, char **blocks)
+{
+	int i;
+	int	start;
+	int position;
+
+	start = 0;
+	position = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] != c && s[i] != '\0')
+		{
+			i++;
+			if (s[i] == c || s[i] == '\0')
+			{
+				
+			}
+		}
+	}
 }
 
 char **ft_split(char const *s, char c)
